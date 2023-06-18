9266446a669e8abed46c2107f66549577810dcbc
Adrian <adrianlam2003@gmail.com>
2023-06-18 12:21:54 -0700

    Created a createTable for SQL

---
+++ src/createTable.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print("Connected to database!")
+
+conn.execute('CREATE TABLE tempTable (name TEXT,transcription TEXT, notes TEXT, date DATE )')
+print("Table created successfully")
+
+conn.close()
