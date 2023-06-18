03f8ba9a5b97a82c5f774a815158d2fa0715c9c7
Your Name <you@example.com>
2023-06-17 21:04:21 -0700

    new backend + flask

--- src/backend.py
+++ src/backend.py
@@ -3,7 +3,52 @@ import openai
 from pydub import AudioSegment
 from moviepy.editor import VideoFileClip, AudioFileClip
 import speech_recognition as sr
-from app import app
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/convert', methods=['POST'])
+def convert_file():
+    # Check if a file was uploaded
+    if 'file' not in request.files:
+        return jsonify(error='No file part in the request'), 400
+
+    file = request.files['file']
+
+    # If the user does not select a file, the browser might
+    # submit an empty file part without a filename, so check for this case
+    if file.filename == '':
+        return jsonify(error='No selected file'), 400
+
+    # Save the file to a temporary location
+    temp_file_path = os.path.join('/tmp', file.filename)
+    file.save(temp_file_path)
+
+    # Convert the file to text
+    text = convert_to_text(temp_file_path)
+
+    # Convert the text to notes
+    notes = convert_to_notes(text)
+
+    # Clean up the temporary file
+    os.remove(temp_file_path)
+
+    # Return the notes as a JSON response
+    return jsonify(notes=notes)
+
+def convert_to_text(file_path):
+    # This function should take a file path (pointing to an MP3, MP4, or text file)
+    # and return the contents of the file as text.
+    # You'll need to fill in the details here.
+    pass
+
+def convert_to_notes(text):
+    # This function should take a string of text and use OpenAI to convert it to notes.
+    # You'll need to fill in the details here.
+    pass
+
+if __name__ == '__main__':
+    app.run(port=5000)
 
 def save_audio(audio_file_path):
     audio = AudioSegment.from_mp3(audio_file_path)
