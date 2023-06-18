e460e69a62ba2a4548b3ec4adc84fd42a5a92ebf
Adrian <adrianlam2003@gmail.com>
2023-06-18 12:16:10 -0700

    Fixed Will's openAI API Code

--- src/backend.py
+++ src/backend.py
@@ -1,94 +1,33 @@
-import os
+from dotenv import load_dotenv
 import openai
-from pydub import AudioSegment
-from moviepy.editor import VideoFileClip, AudioFileClip
-import speech_recognition as sr
-from flask import Flask, request, jsonify
+import os
+import json
 
-app = Flask(__name__)
+load_dotenv()
 
-@app.route('/convert', methods=['POST'])
-def convert_file():
-    # Check if a file was uploaded
-    if 'file' not in request.files:
-        return jsonify(error='No file part in the request'), 400
+# Set OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-    file = request.files['file']
+# Transcribe the audio file
+# Need to LINK the file directory
+audio_file= open("/Users/adrianlam/Downloads/noc.mp3", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-    # If the user does not select a file, the browser might
-    # submit an empty file part without a filename, so check for this case
-    if file.filename == '':
-        return jsonify(error='No selected file'), 400
+transcription = transcript
 
-    # Save the file to a temporary location
-    temp_file_path = os.path.join('/tmp', file.filename)
-    file.save(temp_file_path)
+# Generate notes using OpenAI's API
+prompt = "Create a bullet point note sheet of" + str(transcription)
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+notes = response.choices[0].text.strip()
 
-    # Convert the file to text
-    text = convert_to_text(temp_file_path)
+with open('notes.txt', 'w') as f:
+    f.write(str(notes)) #returns a .txt file 
 
-    # Convert the text to notes
-    notes = convert_to_notes(text)
-
-    # Clean up the temporary file
-    os.remove(temp_file_path)
-
-    # Return the notes as a JSON response
-    return jsonify(notes=notes)
-
-def convert_to_text(file_path):
-    # This function should take a file path (pointing to an MP3, MP4, or text file)
-    # and return the contents of the file as text.
-    # You'll need to fill in the details here.
-    pass
-
-def convert_to_notes(text):
-    # This function should take a string of text and use OpenAI to convert it to notes.
-    # You'll need to fill in the details here.
-    pass
-
-if __name__ == '__main__':
-    app.run(port=5000)
-
-def save_audio(audio_file_path):
-    audio = AudioSegment.from_mp3(audio_file_path)
-    print(f"Loaded audio file of duration {len(audio)} ms")
-
-def save_video(video_file_path):
-    video = VideoFileClip(video_file_path)
-    print(f"Loaded video file of duration {video.duration} s")
-
-def convert_mp4_to_mp3(mp4_file_path, mp3_file_path):
-    audio = AudioFileClip(mp4_file_path)
-    audio.write_audiofile(mp3_file_path)
-
-def transcribe_audio(audio_file_path):
-    r = sr.Recognizer()
-    with sr.AudioFile(audio_file_path) as source:
-        audio_data = r.record(source)
-        text = r.recognize_google(audio_data)
-        return text
-
-def send_to_chatGPT(user_input):
-    messages = [
-        {'role': 'system', 'content': 'You are a tutor. Your job is to create notes based on the text I give you.'},
-        {'role': 'user', 'content': user_input}
-    ]
-    response = openai.ChatCompletion.create(
-        model="gpt-4.0",
-        messages=messages
-    )
-    return response
-
-def get_chat_response(user_input):
-    response = send_to_chatGPT(user_input)
-    chatGPT_response = response.choices[0].message.content
-    return chatGPT_response
-
-# Usage
-save_audio('input.mp3')
-save_video('input.mp4')
-convert_mp4_to_mp3('input.mp4', 'output.mp3')
-transcribed_text = transcribe_audio('output.mp3')
-chat_response = get_chat_response(transcribed_text)
-print(chat_response)
