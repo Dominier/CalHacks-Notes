38265d880ca87fbb0ab677c3d73c848e2d4649b1
Adrian <adrianlam2003@gmail.com>
2023-06-17 20:33:49 -0700

    added flask app name

--- src/flask.py
+++ src/flask.py
@@ -1,3 +1,8 @@
 from flask import Flask, request, render_template, redirect
 import sqlite3
+import os
+
+app = Flask("Notes App")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
